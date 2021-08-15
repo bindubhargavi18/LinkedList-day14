16f0cd1bf9534f67731a7e08a86d07e45682bc55
bindubhargavi568 <bindubhargavi568@gmail.com>
2021-08-15 12:12:17 +0530

    Added:insesrted element 40

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -8,10 +8,11 @@ public class LinkedListMain
 		LinkedList<Integer> list=new LinkedList<>();
 		//appending data is adding of elements..
 		list.add(56);
-		list.add(70);
+		list.add(30);
 		//as index starts from 0, 30 is inserted in between 56 and 70
 		//by using index value..
-		list.insertAt(1, 30);
+		list.add(70);
+		list.insertAt(2, 40); //inserted 40 after 30
 		list.display();
 		int ele=30;
 		if(list.search(ele))
@@ -20,3 +21,4 @@ public class LinkedListMain
 			System.out.println("ELement not found");
 	}
 }
+		
